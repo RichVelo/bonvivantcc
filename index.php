425f054c8e95fad06a44388a7b1674e6211ef527
RichVelo <richrar@gmail.com>
2022-07-12 17:54:56 +0100

    setup

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+?>
+
+<html lang="en-GB">
+<head>
+    <title>Bon Vivant CC</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <header>
+
+    </header>
+    <nav>
+
+    </nav>
+    <main>
+
+    </main>
+
+</body>
+</html>
